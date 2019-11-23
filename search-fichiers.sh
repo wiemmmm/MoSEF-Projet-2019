f250d60f37725d5b5d2043aa4f99dfdd94f96063
wiemmm <wiem.gh88@gmail.com>
2019-11-23 16:52:16 +0100

    Ajout des modification

--- search-fichiers.sh
+++ search-fichiers.sh
@@ -5,8 +5,9 @@ date=$(date '+%d %B %Y')
 hour=$(date '+%H hour %M minutes and %S seconds')
 
 echo "Welcome $usename, today is $date and it is exactly $hour "
-echo "Which repository your're interested in ? :"
+echo "Which repository your're interested in ?"
 read repository
 
 echo "You have choose as repository" $repository
-
+read -p "Which pattern your 're looking for? " pattern
+ls $repository/$pattern 
