a220e1958dada3c7318f703ebba90a71b6d45710
wiemmmm <56681548+wiemmmm@users.noreply.github.com>
2019-11-23 15:36:14 +0100

    Delete search_fichiers.sh

--- search_fichiers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-read -p "What is your name: " username
-date=$(date '+%d %B %Y')
-hour=$(date '+%H heures %M minutes et %S secondes')
-
-echo "Welcome $username, Today is $date and it is exactly $hour "
-echo  "Which repository you're interested in ? "
-read repository
